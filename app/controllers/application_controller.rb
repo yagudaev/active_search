36ba980e5ea4ed9507f9c4a07133bfa222aa3baa
Michael Yagudaev <michaelyagudaev@gmail.com>
2020-11-08 12:08:53 -0800

    fix broken back button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  after_action :avoid_caching_js
+
+  private
+
+  def avoid_caching_js
+    if request.xhr?
+      response.headers['Cache-Control'] = 'no-cache, no-store, max-age=0, must-revalidate'
+      response.headers['Pragma'] = 'no-cache'
+      response.headers['Expires'] = 'Fri, 01 Jan 1990 00:00:00 GMT'
+    end
+  end
 end
